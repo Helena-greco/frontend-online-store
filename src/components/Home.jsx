dbc5b8e4a992663c92a09e7140b9492b26b1a4c5
Helena <nenafgreco@hotmail.com>
2021-09-17 18:52:27 -0300

    Requisito 4 completo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
+// Luana Moneró, Helena Greco e Lucas Pine
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const fetchResult = await getCategories();
+    this.setState({
+      loading: false,
+      categories: fetchResult,
+    });
+  }
+
   render() {
+    const { categories, loading } = this.state;
+    if (loading) { return 'Carregando...'; }
+
     return (
-      <h1 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h1>
+      <>
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+        <sidebar>
+          <ul>
+            { categories.map((category) => (
+              <li key={ category.id } data-testid="category">{ category.name }</li>
+            ))}
+          </ul>
+        </sidebar>
+      </>
     );
   }
 }
